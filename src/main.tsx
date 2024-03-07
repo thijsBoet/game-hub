f77ca1b75a274dcaadf6bdb2672c1fe690094387
Matthijs Boet <m.boet@chello.nl>
2024-03-07 13:58:11 +0100

    setting up fetching

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App'
-import theme from './theme'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
